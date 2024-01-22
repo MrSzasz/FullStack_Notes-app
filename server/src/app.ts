962f2e8a537d28fb7d3f707410191aca217cae8e
Tomas Lugo <lugoleandro@hotmail.com>
2024-01-22 16:51:26 -0300

    cors test

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,7 @@ const app = express()
 // Middlewares
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: process.env.URL_ORIGIN_FOR_CORS,
-    credentials: true,
-  }),
-)
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
