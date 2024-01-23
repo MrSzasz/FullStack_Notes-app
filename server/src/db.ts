d51e87580859a196c39f3cce8a2d588120fa2c74
Tomas Lugo <lugoleandro@hotmail.com>
2024-01-23 01:32:13 -0300

    db changed

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,5 +2,13 @@ import { createPool } from 'mysql2/promise'
 import 'dotenv/config'
 
 export const pool = createPool({
-  uri: process.env.MYSQL_ADDON_URI,
+  host: process.env.MYSQL_DB_HOST,
+  user: process.env.MYSQL_DB_USER,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  port: Number(process.env.MYSQL_DB_PORT),
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.MYSQL_DB_SSL_CA,
+  },
 })
